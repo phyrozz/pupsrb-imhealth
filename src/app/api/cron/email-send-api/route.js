27263f01e9a0c484a9c6ebb74c374c4276d9b4d6
phyrozz <joshuamalabanan70@gmail.com>
2024-02-07 13:32:20 +0800

    updated cron test

--- src/app/api/cron/email-send-api/route.js
+++ src/app/api/cron/email-send-api/route.js
@@ -1,23 +1,31 @@
 import { NextResponse } from "next/server"
+import { headers } from "next/headers"
 // import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
 import { Resend } from "resend"
 
-const resend = new Resend('re_9TqgCnkH_5xKweh4GJw5Wt5uc5MLatUqR')
+const resend = new Resend(process.env.RESEND_API_KEY)
 export const dynamic = 'force-dynamic'
 
 export async function GET() {
   try {
-    resend.emails.send({
+    const headersList = headers()
+    const authHeader = headersList.get('authorization')
+
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+      return NextResponse.json({message: "GET"}, {status: 401})
+    }
+
+    await resend.emails.send({
       from: 'onboarding@resend.dev',
       to: 'joshuamalabanan70@gmail.com',
       subject: 'Hello, world!',
       html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
     })
 
-    return NextResponse.ok()
+    return NextResponse.json({message: "GET"}, {status: 200})
   } catch (error) {
     console.error(error)
-    return NextResponse.error("Error invoking CRON job.")
+    return NextResponse.json({message: "Error: cannot invocate CRON job."})
   }
   
 }
