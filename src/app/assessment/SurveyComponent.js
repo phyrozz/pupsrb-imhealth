8501ff7f4c857d1bc77dd0ea8486f65abfd83a74
Phyrozz <joshuamalabanan70@gmail.com>
2024-03-02 22:48:46 +0800

    fixed survey oncomplete validation

--- src/app/assessment/SurveyComponent.js
+++ src/app/assessment/SurveyComponent.js
@@ -1,11 +1,12 @@
 "use client"
-import React, { useCallback } from "react";
-import { Model } from "survey-core";
-import { Survey } from "survey-react-ui";
-import "survey-core/defaultV2.min.css";
-import { themeJson } from "./theme";
-import { json } from "./json";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import React from "react"
+import { Model } from "survey-core"
+import { Survey } from "survey-react-ui"
+import "survey-core/defaultV2.min.css"
+import { themeJson } from "./theme"
+import { json } from "./json"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { CircularProgress } from "@nextui-org/react"
 
 export default function SurveyComponent({ session }) {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
@@ -13,11 +14,37 @@ export default function SurveyComponent({ session }) {
   const supabase = createClientComponentClient(supabaseUrl, supabaseKey)
   
   const user = session?.user
-  const survey = new Model(json);
+  const survey = new Model(json)
 
-  survey.applyTheme(themeJson);
+  const [allow, setAllow] = React.useState(false)
+  const [isLoading, setIsLoading] = React.useState(true)
 
-  const onComplete = useCallback(async (sender) => {
+  survey.applyTheme(themeJson)
+
+  const getCurrentUserData = React.useCallback(
+    async () => {
+      try {
+        const currentUserData = await supabase.from("assessment_reminders").select(`reminder_sent`).eq('id', user?.id)
+
+        if (currentUserData.data.length === 0) {
+          setAllow(true)
+        } else {
+          setAllow(currentUserData.data.map((index) => (index.reminder_sent))[0])
+        }
+        setIsLoading(false)
+      } catch (e) {
+        console.error("Error retrieving user data: ", e)
+      }
+    },
+    [],
+  )
+  
+  React.useEffect(() => {
+    getCurrentUserData()
+  }, [getCurrentUserData])
+  
+
+  const onComplete = React.useCallback(async (sender) => {
     const results = sender.data
 
     try {
@@ -33,31 +60,41 @@ export default function SurveyComponent({ session }) {
       ])
 
       if (assessmentError) {
-        throw new Error("Error inserting assessment:", assessmentError.message);
+        throw new Error("Error inserting assessment:", assessmentError.message)
       } else {
-        console.log("Assessment inserted successfully:", assessmentData);
+        console.log("Assessment inserted successfully:", assessmentData)
       }
 
       // Upsert assessment reminder into assessment_reminders table
       const { data: reminderData, error: reminderError } = await supabase.from("assessment_reminders").upsert(
         {
-          user_id: user?.id,
           last_assessment_at: new Date().toISOString(), // Current timestamp
           reminder_sent: false,
-        },
+        }
       )
 
       if (reminderError) {
-        throw new Error("Error upserting assessment reminder:", reminderError.message);
+        throw new Error("Error upserting assessment reminder:", reminderError.message)
       } else {
-        console.log("Assessment reminder upserted successfully:", reminderData);
+        console.log("Assessment reminder upserted successfully:", reminderData)
       }
     } catch (error) {
-      console.error("Error:", error.message);
+      console.error("Error:", error.message)
     }
   }, [supabase, user?.id])
 
-  survey.onComplete.add(onComplete);
+  survey.onComplete.add(onComplete)
 
-  return <Survey model={survey} />;
+  return (
+    <>
+      {isLoading ? <div className="h-96 w-screen flex flex-col justify-center items-center">
+        <CircularProgress />
+      </div> : allow ? <Survey model={survey} /> : 
+      <div className="h-96 w-screen flex flex-col justify-center items-center text-slate-900">
+        You have already answered the form. You can go back to this page two weeks after answering your previous response.
+      </div>}
+      
+    </>
+  )
+  
 }
