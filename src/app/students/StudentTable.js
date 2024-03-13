ca6dc416f22a4abc86317ed3b92050b78936d3cf
Joshua Malabanan <joshuamalabanan70@gmail.com>
2024-03-13 21:37:27 +0800

    async pagination on student table

--- src/app/students/StudentTable.js
+++ src/app/students/StudentTable.js
@@ -1,18 +1,20 @@
 "use client"
-import React from "react"
-import {
-  Table,
-  TableHeader,
-  TableColumn,
-  TableBody,
-  TableRow,
-  TableCell,
-  Pagination,
-  CircularProgress,
-} from "@nextui-org/react"
+import React from 'react'
+import { 
+  Table, 
+  TableHeader, 
+  TableColumn, 
+  TableBody, 
+  TableRow, 
+  TableCell, 
+  Pagination, 
+  CircularProgress, 
+  Input
+} from '@nextui-org/react'
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 import { motion } from "framer-motion"
 import StudentHistorySidebar from "./student-history-sidebar"
+import { SearchIcon } from '../components/search-icon'
 
 export default function StudentTable() {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
@@ -23,43 +25,51 @@ export default function StudentTable() {
   const [isLoading, setIsLoading] = React.useState(true)
   const [students, setStudents] = React.useState([])
   const [page, setPage] = React.useState(1)
+  const [studentCount, setStudentCount] = React.useState(0)
   const [selectedUser, setSelectedUser] = React.useState(null)
   const [isOpen, setIsOpen] = React.useState(false)
+  const [searchQuery, setSearchQuery] = React.useState('')
   const rowsPerPage = 20
 
-  const pages = Math.ceil(students.length / rowsPerPage)
+  const pages = Math.ceil(studentCount / rowsPerPage)
 
-  const items = React.useMemo(() => {
-    const start = (page - 1) * rowsPerPage
-    const end = start + rowsPerPage
+  React.useEffect(() => {
+    getStudents()
+  }, [page, searchQuery])
 
-    return students.slice(start, end)
-  }, [page, students])
-
-  const getStudents = React.useCallback(async () => {
+  const getStudents = async () => {
     try {
-      const { data, error, status } = await supabase
-        .from("personal_details")
+      const start = (page - 1) * rowsPerPage
+      const end = start + rowsPerPage
+
+      let query = supabase.from("personal_details")
         .select(
-          `email, first_name, middle_name, last_name, name_suffix, created_at, birth_date, programs ( initial, name ), year, marital_statuses ( status ), is_working_student, profiles ( id, is_student )`
+          `email, first_name, middle_name, last_name, name_suffix, created_at, birth_date, programs ( initial, name ), year, marital_statuses ( status ), is_working_student, profiles ( id, is_student )`,
+          { count: 'exact' }
         )
         .eq("profiles.is_student", true)
+        .range(start, end)
+
+      if (searchQuery) {
+        query = query.or(
+          `first_name.ilike.%${searchQuery}%, last_name.ilike.%${searchQuery}%, email.ilike.%${searchQuery}%`
+        )
+      }        
+
+      const { data, count, error, status } = await query
 
       if (error && status !== 406) {
         throw error
       }
 
+      setStudentCount(count)
       setStudents(data)
     } catch (e) {
       console.error(e)
     } finally {
       setIsLoading(false)
     }
-  }, [supabase])
-
-  React.useEffect(() => {
-    getStudents()
-  }, [getStudents])
+  }
 
   const handleRowClick = (user) => {
     setSelectedUser(user)
@@ -70,6 +80,10 @@ export default function StudentTable() {
     setIsOpen(false)
   }
 
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value)
+  }
+
   return (
     <>
       {isLoading ? (
@@ -77,65 +91,27 @@ export default function StudentTable() {
           <CircularProgress aria-label="Loading..." />
         </div>
       ) : (
-        <div className={isOpen ? "grid grid-cols-12 gap-3" : ""}>
-          <motion.div
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              duration: 0.5,
-              ease: [0, 0.71, 0.2, 1.01],
-            }}
-            className="md:col-span-8 col-span-12 min-h-[90.3vh]"
-          >
-            <Table
-              isStriped
-              aria-label="Student table"
-              selectionMode="single"
-              bottomContent={
-                <div className="flex w-full justify-center">
-                  <Pagination
-                    isCompact
-                    showControls
-                    showShadow
-                    color="secondary"
-                    page={page}
-                    total={pages}
-                    onChange={(page) => setPage(page)}
-                  />
-                </div>
+        <>
+          <div className="w-full pb-3 flex flex-row justify-end">
+            <Input
+              isClearable
+              variant="bordered"
+              radius="md"
+              className="w-96"
+              classNames={{
+                inputWrapper: [
+                  "border-slate-50"
+                ]
+              }}
+              placeholder="Search for student..."
+              value={searchQuery}
+              onChange={handleSearch}
+              startContent={
+                <SearchIcon className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
               }
-              className="text-slate-900"
-            >
-              <TableHeader>
-                <TableColumn>Name</TableColumn>
-                <TableColumn>Email</TableColumn>
-                <TableColumn>Birth Date</TableColumn>
-                <TableColumn>Program</TableColumn>
-                <TableColumn>Year</TableColumn>
-                <TableColumn>Marital Status</TableColumn>
-                <TableColumn>Working Student?</TableColumn>
-              </TableHeader>
-              <TableBody items={items} emptyContent={"No rows to display."}>
-                {(item) => (
-                  <TableRow
-                    key={item.profiles.id}
-                    onClick={() => handleRowClick(item)}
-                  >
-                    <TableCell>{`${item.first_name} ${item.middle_name} ${item.last_name} ${item.name_suffix}`}</TableCell>
-                    <TableCell>{item.email}</TableCell>
-                    <TableCell>{new Date(item.birth_date).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })}</TableCell>
-                    <TableCell>{item.programs.initial}</TableCell>
-                    <TableCell>{item.year || "-"}</TableCell>
-                    <TableCell>{item.marital_statuses.status}</TableCell>
-                    <TableCell>
-                      {item.is_working_student ? "Yes" : "No"}
-                    </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </motion.div>
-          {isOpen && (
+            />
+          </div>
+          <div className={isOpen ? "grid grid-cols-12 gap-3" : ""}>
             <motion.div
               initial={{ opacity: 0, scale: 0.5 }}
               animate={{ opacity: 1, scale: 1 }}
@@ -143,15 +119,74 @@ export default function StudentTable() {
                 duration: 0.5,
                 ease: [0, 0.71, 0.2, 1.01],
               }}
-              className="md:col-span-4 col-span-12 md:static fixed md:w-full w-[94.5vw]"
+              className="md:col-span-8 col-span-12 min-h-[90.3vh]"
             >
-              <StudentHistorySidebar
-                user={selectedUser}
-                onClose={handleCloseSidebar}
-              />
+              <Table
+                isStriped
+                aria-label="Student table"
+                selectionMode="single"
+                bottomContent={
+                  <div className="flex w-full justify-center">
+                    <Pagination
+                      isCompact
+                      showControls
+                      showShadow
+                      color="secondary"
+                      page={page}
+                      total={pages}
+                      onChange={(page) => setPage(page)}
+                    />
+                  </div>
+                }
+                className="text-slate-900"
+              >
+                <TableHeader>
+                  <TableColumn>Name</TableColumn>
+                  <TableColumn>Email</TableColumn>
+                  <TableColumn>Birth Date</TableColumn>
+                  <TableColumn>Program</TableColumn>
+                  <TableColumn>Year</TableColumn>
+                  <TableColumn>Marital Status</TableColumn>
+                  <TableColumn>Working Student?</TableColumn>
+                </TableHeader>
+                <TableBody emptyContent={"No rows to display."}>
+                  {students.map((item) => (
+                    <TableRow
+                      key={item.profiles.id}
+                      onClick={() => handleRowClick(item)}
+                    >
+                      <TableCell>{`${item.first_name} ${item.middle_name} ${item.last_name} ${item.name_suffix}`}</TableCell>
+                      <TableCell>{item.email}</TableCell>
+                      <TableCell>{new Date(item.birth_date).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })}</TableCell>
+                      <TableCell>{item.programs.initial}</TableCell>
+                      <TableCell>{item.year || "-"}</TableCell>
+                      <TableCell>{item.marital_statuses.status}</TableCell>
+                      <TableCell>
+                        {item.is_working_student ? "Yes" : "No"}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
             </motion.div>
-          )}
-        </div>
+            {isOpen && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  duration: 0.5,
+                  ease: [0, 0.71, 0.2, 1.01],
+                }}
+                className="md:col-span-4 col-span-12 md:static fixed md:w-full w-[94.5vw]"
+              >
+                <StudentHistorySidebar
+                  user={selectedUser}
+                  onClose={handleCloseSidebar}
+                />
+              </motion.div>
+            )}
+          </div>
+        </>
       )}
     </>
   )
